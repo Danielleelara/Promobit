49b5bfada5c68d8d067aad13d1ed08db00320020
Danielle Souza <danielle.andrade@meta.com.br>
2022-02-03 14:27:42 -0300

    feat: including category in navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,23 +1,39 @@
-
+import { useState,useEffect } from "react";
+import api from "../api";
 import styles from "./NavBar.module.css"
 import Button from "./Button";
 import Filter from "./Filter";
 import Vector from '../../src/Vector.svg'
-const AppBar = ({movies}) => {
+
+const api_key = "7cbb88dd714f84168b91799865f574d3";
+const NavBar = ({onFilterChange }) => {
+    
+    const [genres, setGenres]= useState([]);
+
+    useEffect(() => {
+        async function getMovieGenres() {
+          const response = await api.get(`/genre/movie/list`, {
+            params: {api_key, language: "pt-BR"}
+          });
+          setGenres(response.data.genres);
+        }
+  
+        getMovieGenres();
+      }, []);
+
     return ( 
     <div className={styles.navbar_container}>
        <div className={styles.navbar_inicio}>
-            <img className={styles.nav_img} src={Vector} alt='logo'/>
+            <img className={styles.nav_img} onClick={()=>{window.history.back()}} src={Vector} alt='logo'/>
        </div>
-    
         <div className={styles.navbar_text}>
             <p className={styles.text1}>Milhões de filmes, séries e pessoas para descobrir. Explore Já </p>
-            <Filter/>
-            <Button movies={movies}/>
+            <Filter />
+            <Button genres={genres} onFilterChange={onFilterChange}/>
         </div>
        
     </div> 
     );
 }
  
-export default AppBar;
+export default NavBar;
